df3ff2c48ea4ec35e4620174183a3bd3ab04f693
paul-ventura-git <j.paul.ventura@gmail.com>
2024-11-29 17:41:28 -0500

    recipient

--- myData.js
+++ myData.js
@@ -1,22 +1,25 @@
 let messages = [
   {
-      id: 1,
-      content: 'Hello World',
-      sender: 'Andres',
-      dateTime: ''
+    id: 1,
+    content: 'Hello World',
+    sender: 'Andres',
+    recipient: 'Me',
+    dateTime: ''
   },
   {
-      id: 2,
-      content: 'Hello Node',
-      sender: 'Ana',
-      dateTime: ''
+    id: 2,
+    content: 'Hello Node',
+    sender: 'Ana',
+    recipient: 'Me',
+    dateTime: ''
   },
   {
     id: 3,
     content: 'Hello Node - Nuevo mensaje',
     sender: 'Andres',
+    recipient: 'Me',
     dateTime: ''
-}
+  }
 ];
 
 module.exports = messages;
